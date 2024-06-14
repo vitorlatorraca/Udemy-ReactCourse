992939ecaf395159006da04e3aa7b6cae074b5cd
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-14 01:15:19 -0400

    new

--- script.js
+++ script.js
@@ -14,13 +14,22 @@ function calcular(event){
 
     if(imc < 17){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = " <br/> Seu resultado foi" + imc + " <br/> Cuidado, voce esta muito magro!";
+        resultado.innerHTML = " <br/> Seu resultado foi " + imc.toFixed(2) + " <br/> Cuidado, voce esta muito magro!";
     }else if(imc > 17 && imc <= 18.49){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = " <br/> Seu resultado foi" + imc + " <br/> Voce ta magro!";
+        resultado.innerHTML = " <br/> Seu resultado foi " + imc.toFixed(2) + " <br/> Voce ta magro!";
     }else if(imc > 18.5 && imc <= 24.99){
         resultado = document.getElementById('resultado');
-        resultado.innerHTML = " <br/> Seu resultado foi" + imc + " <br/> Voce ta na media!";
-    }
+        resultado.innerHTML = " <br/> Seu resultado foi " + imc.toFixed(2) + " <br/> Voce ta na media!";
+    }else if(imc > 25 && imc <= 29.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = " <br/> Seu resultado foi " + imc.toFixed(2) + " <br/> Voce ta um pouco acima da media!";
+    }else if(imc >= 30){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = " <br/> Seu resultado foi " + imc.toFixed(2) + " <br/> Voce ta acima do peso!";
+        }
+
+        document.getElementById('peso').value = '';
+        document.getElementById('altura').value = '';
 
 }
