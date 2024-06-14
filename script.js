ba238a67a6d03f3afcf4eddead6203607194c34d
Vitor Latorraca <vitorlatorraca5@gmail.com>
2024-06-14 01:02:58 -0400

    new

--- script.js
+++ script.js
@@ -6,5 +6,21 @@ var resultado;
 function calcular(event){
     event.preventDefault();
     
-    alert("TESTE")
+
+    peso = document.getElementById('peso').value;
+    altura =document.getElementById('altura').value;
+
+    imc = peso / (altura * altura)
+
+    if(imc < 17){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = " <br/> Seu resultado foi" + imc + " <br/> Cuidado, voce esta muito magro!";
+    }else if(imc > 17 && imc <= 18.49){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = " <br/> Seu resultado foi" + imc + " <br/> Voce ta magro!";
+    }else if(imc > 18.5 && imc <= 24.99){
+        resultado = document.getElementById('resultado');
+        resultado.innerHTML = " <br/> Seu resultado foi" + imc + " <br/> Voce ta na media!";
+    }
+
 }
